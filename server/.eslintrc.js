b89d6e95325bd388006b91af894c4cb4281a4fa8
10fn <chitastrash@yandex.ru>
2023-11-08 17:23:17 +1000

    Сделал базовую настройку настройку.

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -22,4 +22,10 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      extends: ['standard-with-typescript'],
+    },
+  ],
 };
